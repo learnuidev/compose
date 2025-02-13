6f2f04f08aab5bcbd0f7c17d0c805a8650d5d500
learnuidev <learnuidev@gmail.com>
2025-02-13 16:07:37 -0500

    added npm run all command

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,18 +2,23 @@ import { Button } from "@/components/ui/Button";
 
 function App() {
   return (
-    <div className="flex justify-center items-center flex-col mt-32">
-      <h1 className="text-3xl font-bold">compose</h1>
+    <div className="h-screen">
+      <div className="flex justify-center items-center flex-col mt-32">
+        <h1 className="text-3xl font-bold">compose</h1>
 
-      <p className="font-mono mt-4 text-xl">AWS Console for Desktop</p>
+        <p className="font-mono mt-4 text-xl">AWS Console for Desktop</p>
 
-      <Button
-        size={"sm"}
-        variant={"outline"}
-        className="rounded-full mt-8 cursor-pointer"
-      >
-        Discover Compose
-      </Button>
+        <Button
+          size={"sm"}
+          variant={"outline"}
+          className="rounded-full mt-8 cursor-pointer p-4"
+          onClick={() => {
+            alert("composeui");
+          }}
+        >
+          Discover Compose
+        </Button>
+      </div>
     </div>
   );
 }
