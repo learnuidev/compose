7691d69a35f3e612e10b2b7987d1a4b76f5f1213
learnuidev <learnuidev@gmail.com>
2025-02-13 15:13:34 -0500

    install chadcn :)

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/Button";
+
 function App() {
   return (
     <div className="flex justify-center items-center flex-col mt-32">
       <h1 className="text-3xl font-bold">composeui</h1>
 
       <p className="font-mono mt-8 text-xl">AWS Console for Desktop</p>
+
+      <Button size={"sm"} variant={"outline"} className="rounded-full mt-4">
+        Discover
+      </Button>
     </div>
   );
 }
