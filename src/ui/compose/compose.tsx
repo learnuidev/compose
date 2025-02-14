e5d5f181a7467c1098c1991d72758cb89a1d08e1
learnuidev <learnuidev@gmail.com>
2025-02-14 00:08:26 -0500

    added theme toggle

--- src/ui/compose/compose.tsx
+++ src/ui/compose/compose.tsx
@@ -1,18 +1,21 @@
 import { Button } from "@/src/components/ui/button";
 import { useAWSCredentials } from "../hooks/use-aws-credentials";
+import { useTheme } from "@/src/components/theme-provider";
+import { ThggleTheme } from "@/src/components/toggle-theme";
 
 export function Compose() {
+  const { setTheme } = useTheme();
   const stats = useAWSCredentials();
   return (
-    <div className="h-screen">
+    <div className="p-4">
+      <ThggleTheme />
       <div className="flex justify-center items-center flex-col mt-32">
-        <h1 className="text-3xl font-bold">compose</h1>
+        <h1 className="text-3xl sm:text-7xl lg:text-9xl font-bold">compose</h1>
 
-        <p className="font-mono mt-4 text-xl">AWS Console for Desktop</p>
+        <p className="font-mono mt-4 lg:text-5xl sm:text-2xl text-xl dark:text-gray-400">
+          AWS Console for Desktop
+        </p>
 
-        <code>
-          <pre>{JSON.stringify(stats, null, 4)}</pre>
-        </code>
         <Button
           className="rounded-full mt-8 cursor-pointer p-4"
           onClick={() => {
