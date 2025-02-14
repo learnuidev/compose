573d89f302b1c2b8e1da7555d59b8762dd8ec6a1
learnuidev <learnuidev@gmail.com>
2025-02-14 00:14:04 -0500

    wip

--- src/ui/compose/compose.tsx
+++ src/ui/compose/compose.tsx
@@ -1,18 +1,14 @@
-import { Button } from "@/src/components/ui/button";
-import { useAWSCredentials } from "../hooks/use-aws-credentials";
-import { useTheme } from "@/src/components/theme-provider";
 import { ThggleTheme } from "@/src/components/toggle-theme";
+import { Button } from "@/src/components/ui/button";
 
 export function Compose() {
-  const { setTheme } = useTheme();
-  const stats = useAWSCredentials();
   return (
     <div className="p-4">
       <ThggleTheme />
       <div className="flex justify-center items-center flex-col mt-32">
-        <h1 className="text-3xl sm:text-7xl lg:text-9xl font-bold">compose</h1>
+        <h1 className="lg:text-8xl text-5xl font-bold">compose</h1>
 
-        <p className="font-mono mt-4 lg:text-5xl sm:text-2xl text-xl dark:text-gray-400">
+        <p className="lg:text-3xl font-mono text-xl mt-4 lg:mt-8 text-center dark:text-gray-400">
           AWS Console for Desktop
         </p>
 
