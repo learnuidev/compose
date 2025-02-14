a34c37a28a8521d6ee739ebc7952505c82e1576e
learnuidev <learnuidev@gmail.com>
2025-02-13 23:57:49 -0500

    wip

--- src/ui/hooks/use-aws-credentials.ts
+++ src/ui/hooks/use-aws-credentials.ts
@@ -6,7 +6,19 @@ export function useAWSCredentials(): Statistics {
   useEffect(() => {
     const unsub = window.electron.subscribeAWSCredentials((stats) =>
       setValue((prev) => {
-        return stats;
+        return Object.entries(stats)
+          .filter((item) => {
+            const [_, vals] = item;
+            return vals?.aws_access_key_id;
+          })
+          ?.map((item) => {
+            const [profile, credentials] = item;
+            return {
+              profile,
+              awsAccessKeyId: credentials?.aws_access_key_id,
+              awsSecretAccesskey: credentials?.aws_secret_access_key,
+            };
+          });
       })
     );
     return unsub;
